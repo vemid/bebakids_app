57b37af4e9374cfc6ac89de5c90ffe82aa3240ad
MarkoVesic-Windows <admin@bebakids.com>
2023-10-19 23:03:05 +0200

    update2

--- BebaKids/Loyalti/addNewParticipations.Designer.cs
+++ BebaKids/Loyalti/addNewParticipations.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DodavanjeUceniska));
             this.tbCard = new System.Windows.Forms.MaskedTextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -42,17 +43,18 @@
             this.tbId = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.tbArticleNumber = new System.Windows.Forms.TextBox();
-            this.btnAdd = new MaterialSkin.Controls.MaterialFlatButton();
-            this.btnChangePhone = new MaterialSkin.Controls.MaterialFlatButton();
+            this.btnAdd = new MaterialSkin.Controls.MaterialButton();
+            this.btnChangePhone = new MaterialSkin.Controls.MaterialButton();
             this.SuspendLayout();
             // 
             // tbCard
             // 
             this.tbCard.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbCard.Location = new System.Drawing.Point(120, 66);
+            this.tbCard.Location = new System.Drawing.Point(240, 127);
+            this.tbCard.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbCard.Mask = "000000";
             this.tbCard.Name = "tbCard";
-            this.tbCard.Size = new System.Drawing.Size(66, 22);
+            this.tbCard.Size = new System.Drawing.Size(128, 37);
             this.tbCard.TabIndex = 34;
             this.tbCard.Leave += new System.EventHandler(this.tbCard_Leave);
             // 
@@ -60,9 +62,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(10, 100);
+            this.label4.Location = new System.Drawing.Point(20, 192);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 16);
+            this.label4.Size = new System.Drawing.Size(69, 30);
             this.label4.TabIndex = 39;
             this.label4.Text = "Ime :";
             // 
@@ -70,9 +73,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(10, 128);
+            this.label3.Location = new System.Drawing.Point(20, 246);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.Size = new System.Drawing.Size(122, 30);
             this.label3.TabIndex = 38;
             this.label3.Text = "Prezime :";
             // 
@@ -80,35 +84,39 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(10, 71);
+            this.label2.Location = new System.Drawing.Point(20, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.Size = new System.Drawing.Size(160, 30);
             this.label2.TabIndex = 37;
             this.label2.Text = "Broj Kartice :";
             // 
             // tbLastName
             // 
             this.tbLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbLastName.Location = new System.Drawing.Point(120, 122);
+            this.tbLastName.Location = new System.Drawing.Point(240, 235);
+            this.tbLastName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbLastName.Name = "tbLastName";
-            this.tbLastName.Size = new System.Drawing.Size(131, 22);
+            this.tbLastName.Size = new System.Drawing.Size(258, 37);
             this.tbLastName.TabIndex = 36;
             // 
             // tbName
             // 
             this.tbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbName.Location = new System.Drawing.Point(120, 94);
+            this.tbName.Location = new System.Drawing.Point(240, 181);
+            this.tbName.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(131, 22);
+            this.tbName.Size = new System.Drawing.Size(258, 37);
             this.tbName.TabIndex = 35;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 20);
+            this.label1.Size = new System.Drawing.Size(489, 37);
             this.label1.TabIndex = 33;
             this.label1.Text = "Kreiranje nove prijave ucesnika";
             // 
@@ -116,9 +124,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.Location = new System.Drawing.Point(8, 151);
+            this.label8.Location = new System.Drawing.Point(16, 290);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(59, 16);
+            this.label8.Size = new System.Drawing.Size(112, 30);
             this.label8.TabIndex = 41;
             this.label8.Text = "Telefon :";
             // 
@@ -126,74 +135,80 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(8, 179);
+            this.label7.Location = new System.Drawing.Point(16, 344);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(33, 16);
+            this.label7.Size = new System.Drawing.Size(64, 30);
             this.label7.TabIndex = 42;
             this.label7.Text = "Pol :";
             // 
             // tbGender
             // 
             this.tbGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbGender.Location = new System.Drawing.Point(120, 173);
+            this.tbGender.Location = new System.Drawing.Point(240, 333);
+            this.tbGender.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbGender.Name = "tbGender";
-            this.tbGender.Size = new System.Drawing.Size(131, 22);
+            this.tbGender.Size = new System.Drawing.Size(258, 37);
             this.tbGender.TabIndex = 44;
             // 
             // tbPhone
             // 
             this.tbPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbPhone.Location = new System.Drawing.Point(120, 145);
+            this.tbPhone.Location = new System.Drawing.Point(240, 279);
+            this.tbPhone.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbPhone.Name = "tbPhone";
-            this.tbPhone.Size = new System.Drawing.Size(131, 22);
+            this.tbPhone.Size = new System.Drawing.Size(258, 37);
             this.tbPhone.TabIndex = 43;
             // 
             // tbId
             // 
             this.tbId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbId.Location = new System.Drawing.Point(264, 173);
+            this.tbId.Location = new System.Drawing.Point(528, 333);
+            this.tbId.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbId.Name = "tbId";
-            this.tbId.Size = new System.Drawing.Size(131, 22);
+            this.tbId.Size = new System.Drawing.Size(258, 37);
             this.tbId.TabIndex = 46;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(8, 207);
+            this.label5.Location = new System.Drawing.Point(16, 398);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.Size = new System.Drawing.Size(166, 30);
             this.label5.TabIndex = 47;
             this.label5.Text = "Broj Artikala :";
             // 
             // tbArticleNumber
             // 
             this.tbArticleNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbArticleNumber.Location = new System.Drawing.Point(120, 201);
+            this.tbArticleNumber.Location = new System.Drawing.Point(240, 387);
+            this.tbArticleNumber.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbArticleNumber.Name = "tbArticleNumber";
-            this.tbArticleNumber.Size = new System.Drawing.Size(131, 22);
+            this.tbArticleNumber.Size = new System.Drawing.Size(258, 37);
             this.tbArticleNumber.TabIndex = 48;
             // 
             // btnAdd
             // 
             this.btnAdd.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            //this.btnAdd.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnAdd.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
             this.btnAdd.Depth = 0;
-            //this.btnAdd.HighEmphasis = true;
-            //this.btnAdd.Icon = null;
+            this.btnAdd.HighEmphasis = true;
+            this.btnAdd.Icon = null;
             this.btnAdd.Image = global::BebaKids.Properties.Resources.print;
             this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAdd.Location = new System.Drawing.Point(120, 252);
-            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnAdd.Location = new System.Drawing.Point(240, 485);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnAdd.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnAdd.Name = "btnAdd";
-            //this.btnAdd.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnAdd.Size = new System.Drawing.Size(108, 36);
+            this.btnAdd.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnAdd.Size = new System.Drawing.Size(106, 36);
             this.btnAdd.TabIndex = 49;
             this.btnAdd.Text = "      Sacuvaj       ";
             this.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            //this.btnAdd.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            //this.btnAdd.UseAccentColor = false;
+            this.btnAdd.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnAdd.UseAccentColor = false;
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Visible = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
@@ -201,31 +216,31 @@
             // btnChangePhone
             // 
             this.btnChangePhone.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            //this.btnChangePhone.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnChangePhone.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
             this.btnChangePhone.Depth = 0;
-            //this.btnChangePhone.HighEmphasis = true;
-            //this.btnChangePhone.Icon = null;
+            this.btnChangePhone.HighEmphasis = true;
+            this.btnChangePhone.Icon = null;
             this.btnChangePhone.Image = global::BebaKids.Properties.Resources.print;
             this.btnChangePhone.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnChangePhone.Location = new System.Drawing.Point(264, 118);
-            this.btnChangePhone.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnChangePhone.Location = new System.Drawing.Point(528, 227);
+            this.btnChangePhone.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnChangePhone.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnChangePhone.Name = "btnChangePhone";
-            //this.btnChangePhone.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.btnChangePhone.Size = new System.Drawing.Size(153, 36);
+            this.btnChangePhone.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnChangePhone.Size = new System.Drawing.Size(152, 36);
             this.btnChangePhone.TabIndex = 50;
             this.btnChangePhone.Text = "Promeni Telefon";
             this.btnChangePhone.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            //this.btnChangePhone.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            //this.btnChangePhone.UseAccentColor = false;
+            this.btnChangePhone.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnChangePhone.UseAccentColor = false;
             this.btnChangePhone.UseVisualStyleBackColor = true;
             this.btnChangePhone.Click += new System.EventHandler(this.button1_Click);
             // 
             // DodavanjeUceniska
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 313);
+            this.ClientSize = new System.Drawing.Size(872, 602);
             this.Controls.Add(this.btnChangePhone);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label5);
@@ -242,7 +257,8 @@
             this.Controls.Add(this.tbLastName);
             this.Controls.Add(this.tbName);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "DodavanjeUceniska";
             this.Text = "addNewParticipations";
             this.Load += new System.EventHandler(this.addNewParticipations_Load);
@@ -267,7 +283,7 @@
         private System.Windows.Forms.TextBox tbId;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tbArticleNumber;
-        private MaterialSkin.Controls.MaterialFlatButton btnAdd;
-        private MaterialSkin.Controls.MaterialFlatButton btnChangePhone;
+        private MaterialSkin.Controls.MaterialButton btnAdd;
+        private MaterialSkin.Controls.MaterialButton btnChangePhone;
     }
 }
--- BebaKids/Loyalti/addVoucher.Designer.cs
+++ BebaKids/Loyalti/addVoucher.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addVoucher));
             this.label1 = new System.Windows.Forms.Label();
             this.tbDateIskoristivost = new System.Windows.Forms.DateTimePicker();
             this.label6 = new System.Windows.Forms.Label();
@@ -35,27 +36,29 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.tbValue = new System.Windows.Forms.TextBox();
-            this.btnAddVoucer = new System.Windows.Forms.Button();
+            this.btnAddVoucer = new MaterialSkin.Controls.MaterialButton();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Location = new System.Drawing.Point(24, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 20);
+            this.label1.Size = new System.Drawing.Size(385, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "Kreiranje novog vaučera";
             // 
             // tbDateIskoristivost
             // 
             this.tbDateIskoristivost.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.tbDateIskoristivost.Location = new System.Drawing.Point(124, 111);
+            this.tbDateIskoristivost.Location = new System.Drawing.Point(248, 213);
+            this.tbDateIskoristivost.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbDateIskoristivost.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
             this.tbDateIskoristivost.MinDate = new System.DateTime(1950, 1, 1, 0, 0, 0, 0);
             this.tbDateIskoristivost.Name = "tbDateIskoristivost";
-            this.tbDateIskoristivost.Size = new System.Drawing.Size(170, 20);
+            this.tbDateIskoristivost.Size = new System.Drawing.Size(336, 31);
             this.tbDateIskoristivost.TabIndex = 29;
             this.tbDateIskoristivost.Value = new System.DateTime(2022, 12, 1, 0, 0, 0, 0);
             this.tbDateIskoristivost.ValueChanged += new System.EventHandler(this.DatePicker_Changed);
@@ -64,18 +67,20 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(13, 113);
+            this.label6.Location = new System.Drawing.Point(26, 217);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(92, 16);
+            this.label6.Size = new System.Drawing.Size(176, 30);
             this.label6.TabIndex = 28;
             this.label6.Text = "Iskoristivo do :";
             // 
             // tbVoucher
             // 
             this.tbVoucher.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbVoucher.Location = new System.Drawing.Point(124, 73);
+            this.tbVoucher.Location = new System.Drawing.Point(248, 140);
+            this.tbVoucher.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbVoucher.Name = "tbVoucher";
-            this.tbVoucher.Size = new System.Drawing.Size(170, 22);
+            this.tbVoucher.Size = new System.Drawing.Size(336, 37);
             this.tbVoucher.TabIndex = 30;
             this.tbVoucher.Leave += new System.EventHandler(this.tbCard_Leave);
             // 
@@ -83,9 +88,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(14, 79);
+            this.label2.Location = new System.Drawing.Point(28, 152);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.Size = new System.Drawing.Size(175, 30);
             this.label2.TabIndex = 31;
             this.label2.Text = "Broj Vaucera :";
             // 
@@ -93,36 +99,47 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label9.Location = new System.Drawing.Point(14, 153);
+            this.label9.Location = new System.Drawing.Point(28, 294);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(44, 16);
+            this.label9.Size = new System.Drawing.Size(87, 30);
             this.label9.TabIndex = 32;
             this.label9.Text = "Iznos :";
             // 
             // tbValue
             // 
             this.tbValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.tbValue.Location = new System.Drawing.Point(125, 147);
+            this.tbValue.Location = new System.Drawing.Point(250, 283);
+            this.tbValue.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbValue.Name = "tbValue";
-            this.tbValue.Size = new System.Drawing.Size(131, 22);
+            this.tbValue.Size = new System.Drawing.Size(258, 37);
             this.tbValue.TabIndex = 33;
             // 
             // btnAddVoucer
             // 
-            this.btnAddVoucer.Location = new System.Drawing.Point(329, 14);
+            this.btnAddVoucer.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnAddVoucer.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnAddVoucer.Depth = 0;
+            this.btnAddVoucer.HighEmphasis = true;
+            this.btnAddVoucer.Icon = null;
+            this.btnAddVoucer.Location = new System.Drawing.Point(654, 140);
+            this.btnAddVoucer.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnAddVoucer.MouseState = MaterialSkin.MouseState.HOVER;
             this.btnAddVoucer.Name = "btnAddVoucer";
-            this.btnAddVoucer.Size = new System.Drawing.Size(92, 32);
-            this.btnAddVoucer.TabIndex = 34;
+            this.btnAddVoucer.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.btnAddVoucer.Size = new System.Drawing.Size(85, 36);
+            this.btnAddVoucer.TabIndex = 35;
             this.btnAddVoucer.Text = "Sacuvaj";
+            this.btnAddVoucer.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnAddVoucer.UseAccentColor = false;
             this.btnAddVoucer.UseVisualStyleBackColor = true;
-            this.btnAddVoucer.Visible = false;
             this.btnAddVoucer.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // addVoucher
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 215);
+            this.ClientSize = new System.Drawing.Size(995, 413);
             this.Controls.Add(this.btnAddVoucer);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.tbValue);
@@ -131,8 +148,11 @@
             this.Controls.Add(this.tbDateIskoristivost);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "addVoucher";
             this.Text = "addVoucher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.addNewVoucher_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -148,6 +168,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox tbValue;
-        private System.Windows.Forms.Button btnAddVoucer;
+        private MaterialSkin.Controls.MaterialButton btnAddVoucer;
     }
 }
